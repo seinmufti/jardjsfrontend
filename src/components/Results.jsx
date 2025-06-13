ed9fda574c999a14e541de53ccacf3a41376b636
sein <hussein.mufti01@gmail..com>
2025-06-13 18:51:18 +0300

    added loading on api request

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,35 +1,9 @@
 import { Card, Heading, HStack, VStack, Text } from "@chakra-ui/react";
-
-function formatOutput(data, indent = 0) {
-  let outputLines = [];
-  let spaces = "    ".repeat(indent);
-
-  for (let key in data) {
-    let value = data[key];
-    outputLines.push(`${spaces}${key}:`);
-
-    if (typeof value === "object" && !Array.isArray(value)) {
-      for (let k in value) {
-        let v = value[k];
-        if (key.toLowerCase().includes("tubes counts")) {
-          outputLines.push(`${spaces}    ${k}: ${v.toFixed(1)}`);
-        } else if (typeof v === "number" && !Number.isInteger(v)) {
-          outputLines.push(`${spaces}    ${k}: ${v.toFixed(2)}`);
-        } else {
-          outputLines.push(`${spaces}    ${k}: ${v}`);
-        }
-      }
-    } else {
-      outputLines.push(`${spaces}    ${value}`);
-    }
-
-    outputLines.push(""); // Add empty line
-  }
-
-  return outputLines.join("\n");
-}
+import { useMediaQuery } from "@chakra-ui/react";
 
 const Results = ({ mto }) => {
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
+
   return (
     <>
       {Object.keys(mto).length != 0 && (
@@ -38,47 +12,197 @@ const Results = ({ mto }) => {
             <Heading size="md">MTO Results</Heading>
           </Card.Header>
           <Card.Body>
-            <HStack align={"strech"}>
-              <Card.Root size="sm" textAlign="center" w="33%">
-                <Card.Header>
-                  <Heading size="sm">Accessories Counts</Heading>
-                </Card.Header>
-                <Card.Body>
-                  <VStack>
-                    <HStack>
-                      <Text>Cornerjoints</Text>
-                      <Text>
-                        {mto["Structs total accessories counts"].cornerjoint}
-                      </Text>
-                    </HStack>
+            {!isMobile ? (
+              <HStack align={"stretch"}>
+                <Card.Root size="sm" textAlign="center" w="33%">
+                  <Card.Header>
+                    <Heading size="sm">Accessories Counts</Heading>
+                  </Card.Header>
+                  <Card.Body>
+                    <VStack>
+                      <HStack>
+                        <Text>Cornerjoints</Text>
+                        <Text>
+                          {mto["Structs total accessories counts"].cornerjoint}
+                        </Text>
+                      </HStack>
 
-                    <HStack>
-                      <Text>Sliding Sash Handles</Text>
-                      <Text>
-                        {
-                          mto["Structs total accessories counts"][
-                            "sliding sash handle"
-                          ]
-                        }
-                      </Text>
-                    </HStack>
+                      <HStack>
+                        <Text>Sliding Sash Handles</Text>
+                        <Text>
+                          {
+                            mto["Structs total accessories counts"][
+                              "sliding sash handle"
+                            ]
+                          }
+                        </Text>
+                      </HStack>
 
-                    <HStack>
-                      <Text>Sliding Sash Wheels</Text>
-                      <Text>
-                        {
-                          mto["Structs total accessories counts"][
-                            "sliding sash wheel"
-                          ]
-                        }
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </Card.Body>
-              </Card.Root>
+                      <HStack>
+                        <Text>Sliding Sash Wheels</Text>
+                        <Text>
+                          {
+                            mto["Structs total accessories counts"][
+                              "sliding sash wheel"
+                            ]
+                          }
+                        </Text>
+                      </HStack>
+                    </VStack>
+                  </Card.Body>
+                </Card.Root>
 
-              <VStack w="33%">
-                <Card.Root size="sm" textAlign="center" w="100%">
+                <VStack w="33%">
+                  <Card.Root size="sm" textAlign="center" w="100%" h="100%">
+                    <Card.Header>
+                      <Heading size="sm">Structs total area</Heading>
+                    </Card.Header>
+                    <Card.Body>
+                      <Text>{mto["Structs total area"]}</Text>
+                    </Card.Body>
+                  </Card.Root>
+
+                  <Card.Root size="sm" textAlign="center" w="100%" h="100%">
+                    <Card.Header>
+                      <Heading size="sm">Structs total cost</Heading>
+                    </Card.Header>
+                    <Card.Body>
+                      <Text>$ {mto["Structs total cost"]}</Text>
+                    </Card.Body>
+                  </Card.Root>
+                </VStack>
+
+                <Card.Root size="sm" textAlign="center" w="33%">
+                  <Card.Header>
+                    <Heading size="sm">Structs total tubes counts</Heading>
+                  </Card.Header>
+                  <Card.Body>
+                    <VStack>
+                      <HStack>
+                        <Text>Antifly tube</Text>
+                        <Text>
+                          {mto["Structs total tubes counts"][
+                            "Antifly tube"
+                          ].toFixed(2)}
+                        </Text>
+                      </HStack>
+
+                      <HStack>
+                        <Text>Slide L tube</Text>
+                        <Text>
+                          {mto["Structs total tubes counts"][
+                            "Slide L tube"
+                          ].toFixed(2)}
+                        </Text>
+                      </HStack>
+
+                      <HStack>
+                        <Text>Slide Slit tube</Text>
+                        <Text>
+                          {mto["Structs total tubes counts"][
+                            "Slide Slit tube"
+                          ].toFixed(2)}
+                        </Text>
+                      </HStack>
+
+                      <HStack>
+                        <Text>Slide Z tube</Text>
+                        <Text>
+                          {mto["Structs total tubes counts"][
+                            "Slide Z tube"
+                          ].toFixed(2)}
+                        </Text>
+                      </HStack>
+                    </VStack>
+                  </Card.Body>
+                </Card.Root>
+              </HStack>
+            ) : (
+              <VStack align={"stretch"}>
+                <Card.Root size="sm" textAlign="center">
+                  <Card.Header>
+                    <Heading size="sm">Accessories Counts</Heading>
+                  </Card.Header>
+                  <Card.Body>
+                    <VStack>
+                      <HStack>
+                        <Text>Cornerjoints</Text>
+                        <Text>
+                          {mto["Structs total accessories counts"].cornerjoint}
+                        </Text>
+                      </HStack>
+
+                      <HStack>
+                        <Text>Sliding Sash Handles</Text>
+                        <Text>
+                          {
+                            mto["Structs total accessories counts"][
+                              "sliding sash handle"
+                            ]
+                          }
+                        </Text>
+                      </HStack>
+
+                      <HStack>
+                        <Text>Sliding Sash Wheels</Text>
+                        <Text>
+                          {
+                            mto["Structs total accessories counts"][
+                              "sliding sash wheel"
+                            ]
+                          }
+                        </Text>
+                      </HStack>
+                    </VStack>
+                  </Card.Body>
+                </Card.Root>
+
+                <Card.Root size="sm" textAlign="center">
+                  <Card.Header>
+                    <Heading size="sm">Structs total tubes counts</Heading>
+                  </Card.Header>
+                  <Card.Body>
+                    <VStack>
+                      <HStack>
+                        <Text>Antifly tube</Text>
+                        <Text>
+                          {mto["Structs total tubes counts"][
+                            "Antifly tube"
+                          ].toFixed(2)}
+                        </Text>
+                      </HStack>
+
+                      <HStack>
+                        <Text>Slide L tube</Text>
+                        <Text>
+                          {mto["Structs total tubes counts"][
+                            "Slide L tube"
+                          ].toFixed(2)}
+                        </Text>
+                      </HStack>
+
+                      <HStack>
+                        <Text>Slide Slit tube</Text>
+                        <Text>
+                          {mto["Structs total tubes counts"][
+                            "Slide Slit tube"
+                          ].toFixed(2)}
+                        </Text>
+                      </HStack>
+
+                      <HStack>
+                        <Text>Slide Z tube</Text>
+                        <Text>
+                          {mto["Structs total tubes counts"][
+                            "Slide Z tube"
+                          ].toFixed(2)}
+                        </Text>
+                      </HStack>
+                    </VStack>
+                  </Card.Body>
+                </Card.Root>
+
+                <Card.Root size="sm" textAlign="center" w="100%" h="100%">
                   <Card.Header>
                     <Heading size="sm">Structs total area</Heading>
                   </Card.Header>
@@ -87,7 +211,7 @@ const Results = ({ mto }) => {
                   </Card.Body>
                 </Card.Root>
 
-                <Card.Root size="sm" textAlign="center" w="100%">
+                <Card.Root size="sm" textAlign="center" w="100%" h="100%">
                   <Card.Header>
                     <Heading size="sm">Structs total cost</Heading>
                   </Card.Header>
@@ -96,52 +220,7 @@ const Results = ({ mto }) => {
                   </Card.Body>
                 </Card.Root>
               </VStack>
-
-              <Card.Root size="sm" textAlign="center" w="33%">
-                <Card.Header>
-                  <Heading size="sm">Structs total tubes counts</Heading>
-                </Card.Header>
-                <Card.Body>
-                  <VStack>
-                    <HStack>
-                      <Text>Antifly tube</Text>
-                      <Text>
-                        {mto["Structs total tubes counts"][
-                          "Antifly tube"
-                        ].toFixed(2)}
-                      </Text>
-                    </HStack>
-
-                    <HStack>
-                      <Text>Slide L tube</Text>
-                      <Text>
-                        {mto["Structs total tubes counts"][
-                          "Slide L tube"
-                        ].toFixed(2)}
-                      </Text>
-                    </HStack>
-
-                    <HStack>
-                      <Text>Slide Slit tube</Text>
-                      <Text>
-                        {mto["Structs total tubes counts"][
-                          "Slide Slit tube"
-                        ].toFixed(2)}
-                      </Text>
-                    </HStack>
-
-                    <HStack>
-                      <Text>Slide Z tube</Text>
-                      <Text>
-                        {mto["Structs total tubes counts"][
-                          "Slide Z tube"
-                        ].toFixed(2)}
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </Card.Body>
-              </Card.Root>
-            </HStack>
+            )}
           </Card.Body>
         </Card.Root>
       )}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { VStack, Button } from "@chakra-ui/react";
+import { VStack, Button, Spinner } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import MeasurementsCard from "../components/MeasurementsCard";
 import MeasurementInputs from "../components/MeasurementInputs";
@@ -7,9 +7,11 @@ import Results from "../components/Results";
 const HomePage = () => {
   const [measurements, setMeasurements] = useState([]);
   const [mto, setMto] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = () => {
     const cleanedMeasurements = measurements.map(({ index, ...rest }) => rest);
+    setIsLoading(true);
 
     fetch("https://jardjsbackend-production.up.railway.app//jard", {
       method: "POST",
@@ -19,6 +21,7 @@ const HomePage = () => {
       .then((response) => response.json())
       .then((data) => {
         setMto(data.result);
+        setIsLoading(false);
       });
   };
 
@@ -35,7 +38,11 @@ const HomePage = () => {
           handleSubmit={handleSubmit}
         />
 
-        <Results mto={mto} />
+        {isLoading ? (
+          <Spinner color="teal.500" size="lg" />
+        ) : (
+          <Results mto={mto} />
+        )}
       </VStack>
     </>
   );
